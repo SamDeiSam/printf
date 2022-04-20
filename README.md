2106eb01057502eba0193eee15b8b3a79ca0ebff
SamDeiSam <pdeisam@gmail.com>
2022-04-19 17:32:20 -0700

    C - printf

---
+++ README.md
@@ -0,0 +1,4 @@
+# 0x11. C - printf
+
+## Description
+This team project was created by Samuel Dei Pobee and Eric Osafo Adu. The purpose of the project is to replicate the standard "printf" function located in the stdio.h library.
---
+++ print_long_unt.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * prinlunt - prints a long unsigned integer
+ * @arguments: number to print
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printes.
+ */
+int prinlunt(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int_in = va_arg(arguments, unsigned long int);
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, ++)
+	{
+		ibut = handl_buf(buf, ((int_in / div) % 10) + '0' ibuf);
+	}
+	return (i);
+}
